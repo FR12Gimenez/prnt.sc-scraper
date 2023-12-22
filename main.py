78b37e604df4550b6bf9ff3959beb551e56d9e74
Nikee <32960304+FR12Gimenez@users.noreply.github.com>
2023-12-22 14:45:15 +0100

    Update main.py
    Added choice menu with these options:
    1. Check if your IP got banned
    2. Generate images
    3. Exit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
+import requests
 from bs4 import BeautifulSoup
-import os, sys, ctypes, lxml
+import os, sys, ctypes, lxml, time
 from prntsc.generate_urls import generate_urls
 from pystyle import Colors, Colorate, Center
 import concurrent.futures
@@ -23,7 +24,49 @@ logo = """    ____  ____  _   _____________ ______   _____
 logo = Center.XCenter(logo)
 
 def main():
-    print(Colorate.Horizontal(Colors.red_to_blue, logo, 1))
+    
+    while True:
+        print(Colorate.Horizontal(Colors.red_to_blue, logo, 1))
+        print(f"{Fore.CYAN}Choose an option:")
+        print("1. Check if your IP got banned")
+        print("2. Generate images")
+        print("3. Exit")
+        
+        choice = input(f"{Fore.CYAN}> ")
+        
+        if choice == '1':
+            check_ip_banned()
+        elif choice == '2':
+            generate_images()
+        elif choice == '3':
+            break
+        else:
+            print(f"{Fore.RED}Invalid choice. Please enter 1, 2, or 3.")
+
+
+
+def check_ip_banned():
+    url = "https://prnt.sc/"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        status_code = response.status_code
+
+        if status_code == 520: # not banned
+            print(f"{Fore.GREEN}[+] Your IP is not banned on prnt.sc (Status Code: {status_code}).")
+        elif status_code == 403: # banned
+            print(f"{Fore.RED}[!] Your IP may be banned on prnt.sc (Status Code: {status_code}).")
+        else: # i dont know
+            print(f"{Fore.YELLOW}[?] Unexpected status code: {status_code}")
+
+    except requests.RequestException as e: # banned or just not banned
+        print(f"{Fore.RED}[!] IP ban status: {e}") 
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def generate_images():
     print(f"{Fore.CYAN}Enter the number of images to scrape: ")
     num_images = int(input(f"{Fore.CYAN}> "))
     print(f"{Fore.CYAN}Enter the number of threads (3 recommended)")
@@ -45,14 +88,24 @@ def check_dir():
 def load_url(url):
     path = os.getcwd() + "/imgs/"
     file_ext = ".png"
-    req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
-    webpage = urlopen(req).read()
-    soup = BeautifulSoup(webpage, "lxml")
-    img = soup.find("img", {"class": "no-click screenshot-image"}).get("src")
-    if not img.startswith("https://image.prntscr.com/image/"):
-        newurl = generate_urls(1)[0]
-        return load_url(newurl)
-    save_img(img, f"{url[-6:]}{file_ext}", path)
+    
+    try:
+        req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+        webpage = urlopen(req).read()
+        soup = BeautifulSoup(webpage, "lxml")
+        img = soup.find("img", {"class": "no-click screenshot-image"}).get("src")
+        
+        if not img.startswith("https://image.prntscr.com/image/"):
+            newurl = generate_urls(1)[0]
+            return load_url(newurl)
+        
+        save_img(img, f"{url[-6:]}{file_ext}", path)
+        print(f"{Fore.GREEN}[+] Success.\n")
+        
+    except Exception as e:
+        print(f"{Fore.RED}[-] Error loading URL: {e}")
+        print(f"{Fore.YELLOW}[?] It seems your IP may be banned. Exit the program manually please!")
+        time.sleep(60)
 
 def save_img(img, filename, path):
     global valid, invalid
